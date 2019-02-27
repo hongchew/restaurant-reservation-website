4274dcdb33f2b162060e18330bf03fb66519ce01
calyeefornia <e0175177@u.nus.edu>
2019-02-27 14:25:12 +0800

    modified script

--- restaurantScript.sql
+++ restaurantScript.sql
@@ -1,40 +1,40 @@
-DROP TABLE if exists RESTAURANT cascade;
-create schema RESTAURANT;
+DROP SCHEMA RESTAURANT CASCADE;
+CREATE SCHEMA RESTAURANT;
 
-CREATE TABLE RESTAURANT.User(
+CREATE TABLE RESTAURANT.Users(
     email varchar(50) PRIMARY KEY,
-    name varchar(50),
+    username varchar(50),
     password varchar(50),
     accountType varchar(50) NOT null,
     Check(accountType = 'Customer' OR accountType = 'Admin')
 );
 
 CREATE TABLE RESTAURANT.Bookmark(
-    email varchar(50) PRIMARY KEY,
+    email varchar(50) PRIMARY KEY
 );
 
 CREATE TABLE RESTAURANT.Reservation(
-    email varchar(50) PRIMARY KEY,
+    email varchar(50) PRIMARY KEY
 );
 
 CREATE TABLE RESTAURANT.Feedback(
-    email varchar(50) PRIMARY KEY,
+    email varchar(50) PRIMARY KEY
 );
 CREATE TABLE RESTAURANT.Reward(
-    email varchar(50) PRIMARY KEY,
+    email varchar(50) PRIMARY KEY
 );
 CREATE TABLE RESTAURANT.UserReward(
-    email varchar(50) PRIMARY KEY,
+    email varchar(50) PRIMARY KEY
 );
 CREATE TABLE RESTAURANT.Restaurant(
-    email varchar(50) PRIMARY KEY,
+    email varchar(50) PRIMARY KEY
 );
 CREATE TABLE RESTAURANT.Menu(
-    email varchar(50) PRIMARY KEY,
+    email varchar(50) PRIMARY KEY
 );
 CREATE TABLE RESTAURANT.Branch(
-    email varchar(50) PRIMARY KEY,
+    email varchar(50) PRIMARY KEY
 );
 CREATE TABLE RESTAURANT.Vacancy(
-    email varchar(50) PRIMARY KEY,
+    email varchar(50) PRIMARY KEY
 );
