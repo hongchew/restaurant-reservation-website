0101cb28d4b0481ccf59b896c6a997339adc0445
calyeefornia <e0175177@u.nus.edu>
2019-02-27 14:05:56 +0800

    skeleton sql file

--- restaurantScript.sql
+++ restaurantScript.sql
@@ -1,2 +1,40 @@
-DROP TABLE if exists "RestaurantSchema" cascade;
-create schema "RestaurantSchema";
+DROP TABLE if exists RESTAURANT cascade;
+create schema RESTAURANT;
+
+CREATE TABLE RESTAURANT.User(
+    email varchar(50) PRIMARY KEY,
+    name varchar(50),
+    password varchar(50),
+    accountType varchar(50) NOT null,
+    Check(accountType = 'Customer' OR accountType = 'Admin')
+);
+
+CREATE TABLE RESTAURANT.Bookmark(
+    email varchar(50) PRIMARY KEY,
+);
+
+CREATE TABLE RESTAURANT.Reservation(
+    email varchar(50) PRIMARY KEY,
+);
+
+CREATE TABLE RESTAURANT.Feedback(
+    email varchar(50) PRIMARY KEY,
+);
+CREATE TABLE RESTAURANT.Reward(
+    email varchar(50) PRIMARY KEY,
+);
+CREATE TABLE RESTAURANT.UserReward(
+    email varchar(50) PRIMARY KEY,
+);
+CREATE TABLE RESTAURANT.Restaurant(
+    email varchar(50) PRIMARY KEY,
+);
+CREATE TABLE RESTAURANT.Menu(
+    email varchar(50) PRIMARY KEY,
+);
+CREATE TABLE RESTAURANT.Branch(
+    email varchar(50) PRIMARY KEY,
+);
+CREATE TABLE RESTAURANT.Vacancy(
+    email varchar(50) PRIMARY KEY,
+);
