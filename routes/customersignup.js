009756626477364c9ee79054a2ce8a57d847cb3a
calyeefornia <e0175177@u.nus.edu>
2019-04-03 10:18:17 +0800

    customersignup queries before inserting

--- routes/customersignup.js
+++ routes/customersignup.js
@@ -1,38 +1,53 @@
 var express = require('express');
 var router = express.Router();
 
-const { Pool } = require('pg')
+const { Pool } = require('pg');
 const pool = new Pool({
   user: 'postgres',
   host: 'localhost',
   database: 'postgres',
   password: '********',
-  port: 5432,
-})
-
+  port: 5432
+});
 
 router.get('/', function(req, res, next) {
-  res.render('customerSignup', {  });
+  res.render('customerSignup', {emailUsed: false});
 });
 
 // POST
 router.post('/', function(req, res, next) {
-  console.log("test");
-	
-  var name    = req.body.name;
+  console.log('test');
+
+  var name = req.body.name;
   var email = req.body.email;
   var password = req.body.password;
-  var accountType = "customer";
+  var accountType = 'customer';
   console.log(name);
-  var sql_query = 'INSERT INTO RESTAURANT.Users VALUES';
-	
-	// // Construct Specific SQL Query
-	var insert_query = sql_query + "('" + email + "','" + name + "','" + password + "','" + accountType + "')";
-	
-	pool.query(insert_query, (err, data) => {
-    console.log(err);
-		res.redirect('/listrestaurant')
-	});
+  var sql_insert_query = 'INSERT INTO RESTAURANT.Users VALUES';
+  var retrieve_query =
+    "select * from RESTAURANT.Users WHERE email = '" + email + "'";
+
+  var insert_query =
+    sql_insert_query +
+    "('" +
+    email +
+    "','" +
+    name +
+    "','" +
+    password +
+    "','" +
+    accountType +
+    "')";
+  pool.query(retrieve_query, (err, data) => {
+    if (data.rows.length == 0) {
+      pool.query(insert_query, (err, data) => {
+        console.log(err);
+        res.redirect('/listrestaurant');
+      });
+    } else{
+      res.render('customersignup', {emailUsed: true});
+    }
+  });
 });
 
 module.exports = router;
