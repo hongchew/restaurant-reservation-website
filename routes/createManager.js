dfc247d0ae1a5a6fc6f23b2d14cd8cd35c44ff95
Jerome <jeromewjk1996@gmail.com>
2019-04-09 10:13:16 +0800

    create Manager + branch working, messy console checks

--- routes/createManager.js
+++ routes/createManager.js
@@ -4,6 +4,9 @@ const { Pool } = require('pg');
 const pool = new Pool({
     connectionString:process.env.DATABASE_URL
 });
+
+var restaurantName;
+
 router.get('/', function(req, res, next) {
   res.render('createManager');
 });
@@ -11,73 +14,88 @@ router.get('/', function(req, res, next) {
 // POST
 router.post('/', function(req, res, next) {
   var generalManagerEmail = "test1@gmail.com";
-var restaurantName;
-  var getRestaurantNameQuery = "SELECT restaurantName FROM RESTAURANT.Restaurant WHERE generalManagerEmail ='" + generalManagerEmail + "'";
+
+  var getRestaurantNameQuery = "SELECT restaurantName FROM RESTAURANT.Restaurant WHERE generalManagerEmail ='" + generalManagerEmail + "';";
+  console.log("Before insert query for create manager");
   pool.query(getRestaurantNameQuery, (err,data) => {
     if(data.rows.length ==0) {
         console.log("General Manager is not associated with any Restaurants")
     } else{
-        restaurantName = data.rows[0].restaurantName;
+      console.log("Suppose to be restaurant name here ******");
+        restaurantName = data.rows[0].restaurantname;
+        console.log(data.rows);
+        console.log(data.rows[0].restaurantname);
+        console.log(restaurantName);
+        console.log("*****printing body******");
+
+        var name = req.body.name;
+        var email = req.body.email;
+        var branchArea = req.body.brancharea;
+        var password = req.body.password;
+        var accountType = 'Manager';
+        var regionName = req.body.regionname;
+        var address = req.body.address;
+        var openingHour = req.body.openinghour;
+        var closingHour = req.body.closinghour;
+        var capacity = req.body.capacity;
+        console.log("*****printing body******");
+      
+        var retrieve_query =
+        "select * from RESTAURANT.Users WHERE email = '" + email + "';";
+      
+      
+        var insertManagerQuery =
+        "INSERT INTO RESTAURANT.Users VALUES" +
+          "('" +
+          email +
+          "','" +
+          name +
+          "','" +
+          password +
+          "','" +
+          accountType +
+          "');";
+      
+        var insertBranchQuery = "INSERT INTO RESTAURANT.Branch VALUES" + "('" + restaurantName +
+          "','" +
+          branchArea +
+          "','" +
+          regionName +
+          "','" +
+          email +
+          "','" +
+          address +
+          "','" +
+          openingHour +
+          "','" +
+          closingHour +
+          "','" +
+          capacity +
+          "','" +
+          0.0 +
+          "');";
+
+          console.log("before entering insert query");
+          pool.query(retrieve_query, (err, data) => {
+            console.log("****inside insert****");
+            console.log(data.rows);
+            if (data.rows[0] == null) {
+              pool.query(insertManagerQuery, (err, data) => {
+              });
+            }
+              pool.query(insertBranchQuery, (err, data) => {
+                console.log("****trying to insert****");
+                res.redirect('/listrestaurant'); // it should not be here, link to GM's homepage
+              });
+            // } else{
+            //   res.render('createManager');
+            // }
+          });
     }
   });
+  console.log("*****printing body******");
 
-  var name = req.body.name;
-  var email = req.body.email;
-  var branchArea = req.body.brancharea;
-  var password = req.body.password;
-  var accountType = 'Manager';
-  var regionName = req.body.regionname;
-  var address = req.body.address;
-  var openingHour = req.body.openinghour;
-  var closingHour = req.body.closinghour;
-  var capacity = req.body.capacity;
-
-  var retrieve_query =
-  "select * from RESTAURANT.Users WHERE email = '" + email + "'";
-
-
-  var insertManagerQuery =
-  "INSERT INTO RESTAURANT.Users VALUES" +
-    "('" +
-    email +
-    "','" +
-    name +
-    "','" +
-    password +
-    "','" +
-    accountType +
-    "')";
-
-  var insertBranchQuery = "INSERT INTO RESTAURANT.Branch VALUES" + "('" + restaurantName +
-    "','" +
-    branchArea +
-    "','" +
-    regionName +
-    "','" +
-    email +
-    "','" +
-    address +
-    "','" +
-    openingHour +
-    "','" +
-    closingHour +
-    "','" +
-    capacity +
-    "','" +
-    0.0 +
-    "')";
-
-    pool.query(retrieve_query, (err, data) => {
-      if (data.rows.length == 0) {
-        pool.query(insertManagerQuery, (err, data) => {
-        });
-        pool.query(insertBranchQuery, (err, data) => {
-          res.redirect('/listrestaurant');
-        });
-      } else{
-        res.render('createManager');
-      }
-    });
+ 
   
   
 });
