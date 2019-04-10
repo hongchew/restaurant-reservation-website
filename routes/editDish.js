ac731fbb368d5898b9912b1144dc71d025e792b6
Cheng Yang <e0203248@u.nus.edu>
2019-04-10 18:53:40 +0800

    edit dishes without price attribute working

--- routes/editDish.js
+++ routes/editDish.js
@@ -12,7 +12,8 @@ const pool = new Pool({
 router.get('/', function(req, res, next) {
     var menuname = req.query.menuname;
     var restaurantname = req.query.restaurantname;
-
+    console.log(menuname);
+    console.log(restaurantname);
     var query = "select restaurantname, menuname, price, cuisinename from restaurant.menuitem where restaurantname = '" + restaurantname + "' and menuname = '" + menuname + "';";
     console.log(query);
     pool.query(query, (err, data)=>{
@@ -28,13 +29,16 @@ router.get('/', function(req, res, next) {
 
 router.post('/', function(req, res, next) {
 
-    var updatedprice = req.body.updatedprice;
+    var updatedprice = Number(req.body.updatedprice);
     var menuname = req.body.menuname;
     var restaurantname = req.body.restaurantname;
+    console.log(updatedprice);
+    console.log(menuname);
+    console.log(restaurantname);
  
-    var updateQurey = "UPDATE RESTAURANT.MenuItem set price = '" + updatedprice + "' where menuname = '" + menuname + "' and restaurantname = '" + restaurantname+ "';";
-    console.log(query);
-    pool.query(query, (err, data)=>{
+    var updateQuery = "UPDATE RESTAURANT.MenuItem set price = '" + updatedprice + "' where menuname = '" + menuname + "' and restaurantname = '" + restaurantname+ "';";
+    console.log(updateQuery);
+    pool.query(updateQuery, (err, data)=>{
         if(err){
             console.log("***Err EDIT: " + err.message);
         }
